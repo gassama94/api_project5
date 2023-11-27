d7520effef9cbf641549c6115022c99538d8c708
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-27 09:22:20 +0000

    added login/ logout views IsOwner permissions

--- drf_api_p5/settings.py
+++ drf_api_p5/settings.py
@@ -60,6 +60,9 @@ INSTALLED_APPS = [
 
 ]
 
+SITE_ID = 1
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- drf_api_p5/urls.py
+++ drf_api_p5/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from profiles import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    
-      path('profiles/', views.ProfileList.as_view()),
-      path('profiles/<int:pk>', views.ProfileDetail.as_view()),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('profiles.urls')),  
 ]
