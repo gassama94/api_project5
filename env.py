3ab86dfcd36d24c50323e0f9b35fdd1ae4e40d49
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-28 09:45:13 +0000

    Updated DATABASE_URL to point to new ElephantSQL instance

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
 import os 
 
-os.environ['DATABASE_URL'] = 'postgres://dfghqcwu:ArQCbHJK46UkKo-D85oQoO5Mw689DdX8@cornelius.db.elephantsql.com/dfghqcwu'
+#os.environ['DATABASE_URL'] = 'postgres://dfghqcwu:ArQCbHJK46UkKo-D85oQoO5Mw689DdX8@cornelius.db.elephantsql.com/dfghqcwu'
+os.environ['DATABASE_URL'] = 'postgres://mzenalqc:Ox8Z4TXj-GXGx_N-hYR4AXFx3aL-RLXB@cornelius.db.elephantsql.com/mzenalqc'
 
 # 
 os.environ['DEV'] = '1'
