89646261613fedd4cdaaf42a7f46938ad2e274c0
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-28 14:52:41 +0000

    Configured '/dj-rest-auth/login/' to accept POST requests, resolving the 'HTTP 405 Method Not Allowed' error during login.

--- drf_api_p5/settings.py
+++ drf_api_p5/settings.py
@@ -102,6 +102,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'allauth.account.middleware.AccountMiddleware',
@@ -207,6 +208,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
